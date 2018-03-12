e903ff7b917dd9386fa640069a2bf4aa68f1fb7a
Ivan Ramos and Nicko Dela Cruz <nicko@>
2018-03-12 13:18:41 -0700

    initialize files

--- src/main.js
+++ src/main.js
@@ -1,13 +1,17 @@
-import { pingPong } from './ping-pong';
 import './styles.css';
+import { numberOfWords, numberOfVowels, numberOfConsts, getTeaser} from './journal';
 
 $(document).ready(function() {
-  $('#ping-pong-form').submit(function(event) {
+  $('#journal-form').submit(function(event) {
     event.preventDefault();
-    var goal = $('#goal').val();
-    var output = pingPong(goal);
-    output.forEach(function(element) {
-      $('#solution').append("<li>" + element + "</li>");
-    });
+    var entry = $('#entry').val();
+    var output1 = numberOfWords(entry);
+    var output2 = numberOfVowels(entry);
+    var output3 = numberOfConsts(entry);
+    var output4 = getTeaser(entry);
+    $('#solution').append("<p>words : " + output1 + "</p>");
+    $('#solution').append("<p>vowels: " + output2 + "</p>");
+    $('#solution').append("<p>consts: " + output3 + "</p>");
+    $('#solution').append("<p>teaser: " + output4 + "</p>");
   });
 });
